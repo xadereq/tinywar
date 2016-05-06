db79d0ddb19f331c9ef47dc1ee9387c91687868c
xadereq <xadereq@gmail.com>
2016-05-06 04:07:00 +0200

    Fast fix

--- data/creaturescripts/scripts/spamlog.lua
+++ data/creaturescripts/scripts/spamlog.lua
@@ -2,6 +2,7 @@ local condition = createConditionObject(CONDITION_INFIGHT)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 3 * 1000)
 
 function onLogin(cid)
+	if getPlayerGroupId(cid) <= 3 then
 	doAddCondition(cid, condition)
 	return true
 end
